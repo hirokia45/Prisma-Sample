621b4c163580d25a84768b19df21fcc5e06653e5
hirokia45 <hiroki.amada.90@gmail.com>
2021-03-24 17:35:46 +0900

    init

---
+++ index.js
@@ -0,0 +1,18 @@
+const { PrismaClient } = require('@prisma/client')
+
+const prisma = new PrismaClient()
+
+async function main() {
+  const post = await prisma.post.update({
+    where: { id: 3},
+    data: {published: true}
+  })
+
+  console.log(post)
+}
+
+main().catch(e => {
+  throw e
+}).finally(async () => {
+  await prisma.$disconnect
+})
